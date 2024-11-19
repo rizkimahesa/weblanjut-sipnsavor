4396c1ac7fd06ea9aae73e6ef45dfb49f5ac66ea
ImranHamid029 <hamidimrann19@gmail.com>
2024-11-19 14:29:30 +0700

    commit

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\OrderController;
@@ -18,72 +17,42 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-<<<<<<< HEAD
 Route::get('/dashboard', function () {
     return view('home');
-=======
-// Rute Login dan Logout
-Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login')->middleware('guest');
-Route::post('/login', [LoginController::class, 'login'])->middleware('guest');
-Route::post('/logout', function () {
-    Auth::logout();
-    request()->session()->invalidate();
-    request()->session()->regenerateToken();
-    return redirect()->route('login');
-})->name('logout');
-
-// Rute untuk Dashboard User
-Route::middleware(['auth', 'role:user'])->group(function () {
-    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
->>>>>>> login
 });
 
 // Rute untuk Dashboard Admin dan Pengelolaan Menu
 Route::middleware(['auth', 'role:admin'])->group(function () {
     Route::get('/menus', [AdminController::class, 'index'])->name('menus.index');
-    Route::resource('menus', MenuController::class);  // CRUD Menu
+    Route::resource('menus', MenuController::class); // CRUD Menu
 });
 
-// Rute lain
+// Rute untuk Order
 Route::resource('orders', OrderController::class);
-<<<<<<< HEAD
-Route::resource('menus', MenuController::class);
+Route::get('orders/create/{menuId?}', [OrderController::class, 'create'])->name('orders.create');
+
+// Rute untuk Menu
+Route::get('/menus/create', [MenuController::class, 'create'])->name('menus.create');
+Route::post('/menus/store', [MenuController::class, 'store'])->name('menus.store');
 Route::get('/menus/{menu}/edit', [MenuController::class, 'edit'])->name('menus.edit');
 Route::delete('/menus/{menu}', [MenuController::class, 'destroy'])->name('menus.destroy');
-Route::get('orders/create/{menuId?}', [OrderController::class, 'create'])->name('orders.create');
 Route::get('/order', [MenuController::class, 'order'])->name('order');
-=======
->>>>>>> login
-Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
+
+// Rute untuk Cart
 Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
 Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
 Route::post('/cart/checkout', [CartController::class, 'checkout'])->name('cart.checkout');
-<<<<<<< HEAD
-Route::get('/cart', [CartController::class, 'cart'])->name('cart');
 
-Route::get('/menus/create', [MenuController::class, 'create'])->name('menus.create');
-Route::post('/menus/store', [MenuController::class, 'store'])->name('menus.store');
+// Rute untuk Kontak
+Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 
-Route::view('/', 'home')->name('home');
-Route::view('/contact', 'contact')->name('contact');    
-=======
-Route::get('register', [UserController::class, 'showRegistrationForm'])->name('register');
-Route::post('register', [UserController::class, 'register']);
-
-
-// Rute untuk profil
+// Rute untuk Profil
 Route::middleware(['auth'])->group(function () {
     Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
 });
 
-Route::middleware('auth')->group(function () {
-    Route::get('/cart', [CartController::class, 'index'])->name('cart.index');
-    Route::post('/cart', [CartController::class, 'store'])->name('cart.store');
-});
-
-// routes/web.php
+// Rute untuk Autentikasi
 Route::post('/login', [LoginController::class, 'login']);
 Route::get('/home', [HomeController::class, 'index'])->middleware('auth');
 Route::get('register', [RegisteredController::class, 'create'])->name('register');
 Route::post('register', [RegisteredController::class, 'register']);
->>>>>>> login
